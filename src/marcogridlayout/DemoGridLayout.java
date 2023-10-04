0bc8e9e441d57ed38d58518a01d24c3e6b25b5da
MCC-Maritza <M.C.C.Maritza@outlook.es>
2023-10-03 23:31:00 -0600

    Se sube programa

---
+++ src/marcogridlayout/DemoGridLayout.java
@@ -0,0 +1,16 @@
+package marcogridlayout;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author maryse
+ */
+public class DemoGridLayout {
+
+    public static void main(String args[]) {
+        MarcoGridLayout marcoGridLayout = new MarcoGridLayout();
+        marcoGridLayout.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        marcoGridLayout.setSize(300, 200); // establece el tamaño del marco
+        marcoGridLayout.setVisible(true); // muestra el marco
+    } // ﬁn de main
+} // ﬁn de la clase DemoGridLayout
